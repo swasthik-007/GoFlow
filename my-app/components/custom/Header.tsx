3bfeb77daa7db95118c8587f8a23628b5a062c42
Ashis-Mishra07 <mishralucky074@gmail.com>
2025-04-05 01:52:09 +0530

    custom file added to components

---
+++ my-app/components/custom/Header.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+
+import Link from "next/link";
+
+function Header() {
+  
+  return (
+    <div className="flex justify-between items-center p-4 shadow-sm">
+      <div>
+        <div className="flex gap-3 items-center">
+          <Link href={"/dashboard"}></Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
